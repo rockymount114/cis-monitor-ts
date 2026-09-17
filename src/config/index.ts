bc709f93a3f9a688229ef43c840188344ac10731
rockymount114 <rockymount114@gmail.com>
2026-09-17 09:48:27 -0400

    remove default settings

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 
 export const config = {
   db: {
-    server: process.env.DB_SERVER || '172.20.80.12',
-    database: process.env.DB_DATABASE || 'CIS4PROD',
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     user: process.env.DB_USER || '',
     password: process.env.DB_PASSWORD || '',
     options: {
@@ -13,7 +13,7 @@ export const config = {
     },
     pool: { max: 10, min: 0, idleTimeoutMillis: 30000 },
   },
-  cisServer: process.env.CIS_SERVER || '172.20.80.12',
+  cisServer: process.env.CIS_SERVER,
   cisPorts: (process.env.CIS_PORTS || '3389,443,80,81,1433').split(',').map(n => parseInt(n.trim(),10)),
   cronTimes: (process.env.CRON_TIMES || '0 6 * * *,0 11 * * *,20 19 * * *,0 22 * * *').split(',').map(s=>s.trim()),
   notify: {
